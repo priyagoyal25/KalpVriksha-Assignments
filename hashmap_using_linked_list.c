208c659647aaaa9ed6844d16e54309edde2dafe8
Priya Goyal <priya.goyal@intimetec.com>
2025-02-07 15:55:05 +0530

    Change datatype of hashmap

--- hashmap_using_linked_list.c
+++ hashmap_using_linked_list.c
@@ -2,9 +2,10 @@
 #include<stdlib.h>
 #include<string.h>
 
+#define TABLE_SIZE 10
 struct node{
-    char *key;
-    char *value;
+    int key;
+    int value;
     struct node *next;
 };
 
@@ -16,49 +17,39 @@ struct hashmap{
 
 struct hashmap *map = NULL;
 
-struct node* create_node(char *key, char *value){
+struct node* create_node(int key, int value){
     struct node* newnode = (struct node*) malloc(sizeof(struct node));
-    newnode->key = (char*) malloc(sizeof(char)*strlen(key)+1);
-    newnode->value = (char*) malloc(sizeof(char)*strlen(value)+1);
-
-    strcpy(newnode->key, key);
-    strcpy(newnode->value, value);
+    newnode->key = key;
+    newnode->value = value;
     newnode->next = NULL;
     return newnode;
 }
 
 void initialise_hashmap(){
     map = (struct hashmap*) malloc(sizeof(struct hashmap));
-    map->capacity = 100;
+    map->capacity = TABLE_SIZE;
     map->size = 0;
     map->data = (struct node **)calloc(map->capacity, sizeof(struct node*));
 }
 
-int hash_function(char* key){
-    int sum = 0;
-    for(int i=0;key[i] != '\0';i++){
-        sum += key[i];
-    }
-    return sum%map->capacity;
+int hash_function(int key){
+    int index = key % TABLE_SIZE;
+    return index;
 }
 
-char* search(char * key){
+int search(int key){
     int index = hash_function(key);
     struct node* cur_node = map->data[index];
     while(cur_node != NULL){
-        if(strcmp(cur_node->key, key) == 0){
+        if(cur_node->key == key){
             return cur_node->value;
         }
         cur_node = cur_node->next;
     }
-    return NULL;
+    return -1;
 }
 
-void insert_into_hashmap(char *key, char *value){
-    if(search(key) != NULL){
-        printf("\nDuplicate keys are not allowed");
-        return;
-    }
+void insert_into_hashmap(int key, int value){
     int index = hash_function(key);
     struct node* newnode = create_node(key, value);
     
@@ -73,23 +64,21 @@ void insert_into_hashmap(char *key, char *value){
     printf("\nInserted successfully");
 }
 
-void delete_from_hashmap(char* key){
+void delete_from_hashmap(int  key){
     int index = hash_function(key);
     struct node* cur_node = map->data[index];
     struct node* prev = NULL;
 
     while(cur_node != NULL){
-        if(strcmp(cur_node->key, key) == 0){
+        if(cur_node->key == key){
             if(prev == NULL){
                 map->data[index] = cur_node->next;
             }
             else{
                 prev->next = cur_node->next;
             }
-            free(cur_node->key);
-            free(cur_node->value);
             free(cur_node);
-            printf("\nKey deleted");
+            printf("\nKey %d deleted", key);
             map->size--;
             return;
         }
@@ -100,11 +89,18 @@ void delete_from_hashmap(char* key){
 }
 
 void display(){
+    printf("\nHash Table:\n");
     for(int i=0;i<map->capacity;i++){
+        int is_arrow = 0;
         struct node* cur_node = map->data[i];
+        if(cur_node != NULL){
+            printf("\nIndex %d: ", i);
+        }
         while(cur_node != NULL){
-            printf("key: %s value: %s\n", cur_node->key, cur_node->value);
+            if(is_arrow) printf("-> ");
+            printf("(%d, %d) ", cur_node->key, cur_node->value);
             cur_node = cur_node->next;
+            is_arrow = 1;
         }
     }
 }
@@ -114,14 +110,13 @@ int main(){
     initialise_hashmap();
 
     int choice;
-    char* key = (char*) malloc(sizeof(char)*100);
-    char* value = (char*) malloc(sizeof(char)*100);
+    int key, value;
 
     while(1){
         printf("\n\n---MENU---");
         printf("\n1. Insert in hashmap");
-        printf("\n2. Delete in hashmap");
-        printf("\n3. Search in hashmap");
+        printf("\n2. Search in hashmap");
+        printf("\n3. Delete in hashmap");
         printf("\n4. Display hashmap");
         printf("\n5. Exit\n");
 
@@ -134,27 +129,27 @@ int main(){
         switch(choice){
             case 1: 
             printf("\nEnter key: ");
-            scanf("%s", key);
+            scanf("%d", &key);
             printf("Enter value: ");
-            scanf("%s", value);
+            scanf("%d", &value);
             insert_into_hashmap(key, value);
             break;
 
-            case 2: 
-            printf("\nEnter key to delete: ");
-            scanf("%s", key);
-            delete_from_hashmap(key);
-            break;
-
-            case 3:
+            case 2:
             printf("\nEnter key to search: ");
-            scanf("%s", key);
-            char* result = search(key);
-            if(result == NULL){
+            scanf("%d", &key);
+            int result = search(key);
+            if(result == -1){
                 printf("\nKey not found");
                 break;
             }
-            printf("\nCorresponding value: %s", result);
+            printf("\nCorresponding value: %d", result);
+            break;
+
+            case 3: 
+            printf("\nEnter key to delete: ");
+            scanf("%d", &key);
+            delete_from_hashmap(key);
             break;
 
             case 4:
@@ -162,7 +157,7 @@ int main(){
             break;
 
             case 5:
-            printf("Exiting..");
+            printf("Exiting program");
             free(map->data);
             free(map);
             exit(0);
